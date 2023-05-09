595cd2f128e7b7bbe0622d1b8068e46907ca9bcc
Julen Baroja <125016128+julenbaroja@users.noreply.github.com>
2023-05-09 17:23:10 +0200

    8 irudiko funtzioa

---
+++ MTKP_proiektua/8irudia_funtzioa.f90
@@ -0,0 +1,23 @@
+function f2(t,y) result(y4)
+    use mcf_tipos
+real(kind=dp), intent(in)               :: t
+real(kind=dp), dimension(:), intent(in) :: y
+real(kind=dp), dimension(size(y))       :: y4
+real(kind=dp) :: g,alpha,beta,gama,chi,w,A,pi,phi
+
+pi=acos(-1.0_dp)
+alpha=0.07_dp
+beta=1.017_dp
+gama=15.103_dp
+chi=0.00656_dp
+g=9.8_dp
+w=2.0_dp * pi * 1.25_dp
+A=1.16_dp
+phi=pi/6.0_dp
+
+y4(1)=y(2)
+y4(2)=(5.0_dp/7.0_dp)*(-0.001_dp*y(2)-(2.0_dp*alpha*y(1)-beta*y(1)/sqrt(y(1)**2+gama)-chi*y(3))*g+A*w**2*sin(w*t)*cos(phi))
+y4(3)=y(4)
+y4(4)=(5.0_dp/7.0_dp)*((-0.001_dp*y(4)-(2.0_dp*alpha*y(3)-beta*y(3)/sqrt(y(3)**2+gama)-chi*y(1))*g)+A*w**2*sin(w*t)*sin(phi))
+
+end function f2
