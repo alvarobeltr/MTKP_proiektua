b11feb37366b3ef43552a555381ade1a8f3ee570
Julen Baroja <125016128+julenbaroja@users.noreply.github.com>
2023-05-10 14:07:32 +0200

    Hirugarren maiztasunari dagokion desfasea

---
+++ MTKP_proiektua/desfasea3_3.f90
@@ -0,0 +1,36 @@
+program desfasea3_3
+use mcf_tipos
+use funtzioak
+use rk4
+
+integer             :: i,j,g
+real(kind=dp)       :: t, h,a,b,c,d,e
+real(kind=dp), dimension(4)    :: y
+real(kind=dp), dimension(10000):: z
+integer, parameter  :: n=10000 ! h pausu kopurua 
+real(kind=dp), parameter :: ta=0.0_dp,tb =1000.0_dp, pi=acos(-1.0_dp)
+
+h=(tb-ta)/n
+t    =  0.0_dp
+y(1) = -6.556_dp ! hasierako balioak
+y(2) = 0.0_dp
+y(3)= -6.556_dp
+y(4)=0.0_dp
+open(unit=12,file="sollibre.dat",status="replace",action="write")
+
+open(unit=13,file="desfase3_3.dat",status="replace",action="write")
+
+do i=1,n
+ write(unit=12,fmt="(6es20.12)") t, y
+ z(i)=y(1)
+ call rk4_paso_dp(t,y,f2,h)
+end do
+
+do i=1,500
+
+ write(unit=13,fmt="(6es20.12)") z(i), z(i+20)
+
+enddo
+close(unit=12)
+close(unit=13)
+end program desfasea3_3
