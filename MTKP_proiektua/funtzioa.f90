0ae58ca1d607277c62ca764fa1a3e1a02f0270f0
alvarobeltr <124918644+alvarobeltr@users.noreply.github.com>
2023-05-12 09:50:35 +0200

    Update funtzioa.f90

--- MTKP_proiektua/funtzioa.f90
+++ MTKP_proiektua/funtzioa.f90
@@ -14,9 +14,9 @@ gamma=15.103_dp
 chi=0.00656_dp
 g=9.8_dp
 y4(1)=y(2)
-y4(2)=(5.0_dp/7.0_dp)*(-0.3_dp*y(2)-(2.0_dp*alpha*y(1)-beta*y(1)/sqrt(y(1)**2+gamma)-chi*y(3))*g)
+y4(2)=(5.0_dp/7.0_dp)*(-0.0025_dp*y(2)-(2.0_dp*alpha*y(1)-beta*y(1)/sqrt(y(1)**2+gamma)-chi*y(3))*g)
 y4(3)=y(4)
-y4(4)=(5.0_dp/7.0_dp)*(-0.3_dp*y(4)-(2.0_dp*alpha*y(3)-beta*y(3)/sqrt(y(3)**2+gamma)-chi*y(1))*g)
+y4(4)=(5.0_dp/7.0_dp)*(-0.0025_dp*y(4)-(2.0_dp*alpha*y(3)-beta*y(3)/sqrt(y(3)**2+gamma)-chi*y(1))*g)
 
 end function f
 function f2(t,y) result(y4)
@@ -35,9 +35,9 @@ w=1.25_dp
 A=1.16_dp
 phi=pi/6.0_dp
 y4(1)=y(2)
-y4(2)=(5.0_dp/7.0_dp)*(-0.001_dp*y(2)-(2.0_dp*alpha*y(1)-beta*y(1)/sqrt(y(1)**2+gamma)-chi*y(3))*g+A*w**2*sin(w*t)*cos(phi))
+y4(2)=(5.0_dp/7.0_dp)*(-0.0025_dp*y(2)-(2.0_dp*alpha*y(1)-beta*y(1)/sqrt(y(1)**2+gamma)-chi*y(3))*g+A*w**2*sin(w*t)*cos(phi))
 y4(3)=y(4)
-y4(4)=(5.0_dp/7.0_dp)*((-0.001_dp*y(4)-(2.0_dp*alpha*y(3)-beta*y(3)/sqrt(y(3)**2+gamma)-chi*y(1))*g)+A*w**2*sin(w*t)*sin(phi))
+y4(4)=(5.0_dp/7.0_dp)*((-0.0025_dp*y(4)-(2.0_dp*alpha*y(3)-beta*y(3)/sqrt(y(3)**2+gamma)-chi*y(1))*g)+A*w**2*sin(w*t)*sin(phi))
 
 end function f2
 end module funtzioa
